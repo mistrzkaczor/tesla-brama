286dea4274f39976698b822b2a9d78741fd43b41
mistrzkaczor <kozeramariusz@gmail.com>
2026-08-04 14:33:53 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,30 @@
+/* ==========================================
+   Tesla SUPLA Dashboard
+   Konfiguracja
+========================================== */
+
+const CONFIG = {
+
+    // Link do otwarcia bramy
+    OPEN_URL:
+    "https://svr20.supla.org/direct/1630/7ebdAmEowLmG/open",
+
+
+    // Link do zamknięcia bramy
+    CLOSE_URL:
+    "https://svr20.supla.org/direct/1631/Y9wnqZsr7rcm/close",
+
+
+    // Link do odczytu statusu
+    STATUS_URL:
+    "https://svr20.supla.org/direct/1632/hMj8i64QTPFPN",
+
+
+    // Odświeżanie statusu (ms)
+    REFRESH_INTERVAL: 5000,
+
+
+    // Włącz diagnostykę
+    DEBUG: true
+
+};
