97a43456340881af57b65973d7d8fced95b83897
mistrzkaczor <kozeramariusz@gmail.com>
2026-08-04 14:10:40 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,3 @@
+const OPEN_URL = "https://svr20.supla.org/direct/1630/7ebdAmEowLmG/open";
+const CLOSE_URL = "https://svr20.supla.org/direct/1631/Y9wnqZsr7rcm/close";
+const STATUS_URL = "https://svr20.supla.org/direct/1632/hMj8i64QTPFPN";
