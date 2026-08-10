20e6652f96f9df1f2acd8d467696323aecc79d52
mistrzkaczor <kozeramariusz@gmail.com>
2026-08-10 18:17:24 +0200

    Add service worker for caching and request handling

---
+++ service-worker.js
@@ -0,0 +1,70 @@
+const CACHE_NAME = "tesla-gate-v3-4-0";
+
+const APP_FILES = [
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./config.js",
+    "./style.css",
+    "./img/favicon.svg",
+    "./img/gate-open.svg",
+    "./img/gate-close.svg",
+    "./img/icon-192.png",
+    "./img/icon-512.png"
+];
+
+/* Instalacja */
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(APP_FILES))
+    );
+
+    self.skipWaiting();
+});
+
+/* Aktywacja */
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.map((key) => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+
+    self.clients.claim();
+});
+
+/* Obsługa żądań */
+
+self.addEventListener("fetch", (event) => {
+
+    const url = event.request.url;
+
+    /* Nie cachujemy SUPLA */
+
+    if (
+        url.includes("/read?") ||
+        url.includes("/open") ||
+        url.includes("/close")
+    ) {
+        return;
+    }
+
+    event.respondWith(
+        caches.match(event.request)
+            .then((cachedResponse) => {
+                return (
+                    cachedResponse ||
+                    fetch(event.request)
+                );
+            })
+    );
+});
