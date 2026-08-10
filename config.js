379a27ce0e9c4fb8a40ed92ae1ec7713489b6b8e
mistrzkaczor <kozeramariusz@gmail.com>
2026-08-10 09:40:08 +0200

    Update configuration comments and timeout values

--- config.js
+++ config.js
@@ -20,8 +20,11 @@ VERSION: "3.2.1",
     STATUS_URL:
     "https://svr20.supla.org/direct/1634/Wweiib2Y7Pbhxog/read?format=json",
 
-   // Czas wyświetlania komunikatu
+// Czas wyświetlania komunikatu
    MESSAGE_TIMEOUT: 2000,
+
+// Maksymalny czas oczekiwania na odpowiedź
+   FETCH_TIMEOUT: 5000,
 
  // Czas odświeżania statusu
    REFRESH_INTERVAL: 5000,
